2e910415a95181a5253ce1202323049730f293cc
bens1320 <yu1piao09@163.com>
2020-09-29 21:10:11 +0800

    模块化: Money页面模块化为4个组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="请输入备注信息">
+    </label>
+  </div>
+
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background: #f5f5f5;
+  //同外部div 改为block
+  //display: block;
+  font-size: 14px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    line-height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+
+</style>
