45f80e6a5f86d7a8f6274451ab5b6411a4597afd
bens1320 <yu1piao09@163.com>
2020-09-30 09:32:01 +0800

    用 TS 在 Money 页面的 使用Prop

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -10,16 +10,27 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Types extends Vue {
-  type = '-'
-  selectType(type: string){
-    if(type !== '-' && type !== '+'){
-      throw new Error('type is unknown')
+  type = '-';
+
+  //Prop 告诉 Vue, xxx不是 data 是 prop
+  //Number 告诉 Vue, xxx 运行时, 是个Number
+  //xxx 属性名
+  //number | undefined 告诉 TS xxx 的编译时的类型
+  @Prop(Number) xxx: number | undefined;
+
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
     }
-    this.type = type
+    this.type = type;
+  }
+
+  mounted() {
+    console.log(this.xxx);
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad/>
-    <Types xxx="hi"/>
+    <Types :xxx="2233"/>
     <Notes/>
     <Tags/>
   </Layout>
