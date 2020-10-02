40dc3fdb9d4d3f99af0d516001e6983149a3aee6
bens1320 <yu1piao09@163.com>
2020-10-02 15:48:57 +0800

    Label 页面: 将 tag 的 string 改为{id, name}

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <layout>
+      编辑标签
+    </layout>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class EditLabel extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,8 @@
 <template>
   <Layout>
     <ol class="tags">
-      <li v-for="tag in tagList" :key="tag">
-        <span>{{tag}}</span>
+      <li v-for="tag in tagList" :key="tag.id">
+        <span>{{tag.name}}</span>
         <Icon name="right"/>
       </li>
 
