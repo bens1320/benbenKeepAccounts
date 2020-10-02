2a7ed8eda83b22cbb5e5c69f76d9ac9b47c44534
bens1320 <yu1piao09@163.com>
2020-10-02 11:40:11 +0800

    Label 页面: 完成展示和添加标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,17 +14,20 @@ import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component, Watch} from 'vue-property-decorator';
-import model from '@/model.ts';
+import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
+
 
 window.localStorage.setItem('version', '0.0.1');
 
-const recordList = model.fetch();
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
-  tags = ['衣', '食', '住', '行', '彩票'];
+  tags = tagList;
   recordList: RecordItem[] = recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '+', amount: 100
@@ -41,14 +44,14 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2 = model.clone(this.record)
+    const record2 = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
 
   @Watch('recordList')
   onRecordListChange(){
-    model.save(this.recordList)
+    recordListModel.save(this.recordList);
   }
 }
 
