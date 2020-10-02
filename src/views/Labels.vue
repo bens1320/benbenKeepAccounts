c8483806db876375f0ba199d2e27e8a116c84612
bens1320 <yu1piao09@163.com>
2020-10-02 10:32:19 +0800

    Label 页面 HTML 布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,26 @@
 <template>
-    <Layout>
-      <p>
-        Labels.vue
-      </p>
-    </Layout>
+  <Layout>
+    <ol>
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div>
+      <button>添加标签</button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
