83abac0cad878b724c4a43cf542ff06f2e366950
bens1320 <yu1piao09@163.com>
2020-10-04 13:58:17 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,15 +1,22 @@
+import clone from '@/lib/clone';
+
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+  data: [] as RecordItem[],
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data
   },
 
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify((data)));
+  save() {
+    return window.localStorage.setItem(localStorageKeyName,
+      JSON.stringify((this.data)));
   },
 
-  clone(data: RecordItem[] | RecordItem){
-    return JSON.parse(JSON.stringify(data))
+  create(record: RecordItem){
+    const record2 = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
   }
 
 };
