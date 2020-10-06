c0458a19f04913d9d1d24270e80fa7e42af9870d
bens1320 <yu1piao09@163.com>
2020-10-07 01:55:35 +0800

    Lables 使用 Vuex

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
     <div class="tags">
-      <router-link class="tag" v-for="tag in tagList" :key="tag.id"
+      <router-link class="tag" v-for="tag in tags" :key="tag.id"
                    :to="`/labels/edit/${tag.id}`">
-        <span>{{tag.name}}</span>
+        <span>{{ tag.name }}</span>
         <Icon name="right"/>
       </router-link>
 
@@ -20,34 +20,40 @@ import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 
 @Component({
-  components: {Button}
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    }
+  }
 })
 export default class Labels extends Vue {
-  //TODO
-  tags = [] // tagList = store.tagList
+  Created() {
+    this.$store.commit('fetchTags');
+  }
 
-  createTag(){
-    const name = window.prompt('请输入标签名')
-    if(name){
-      //TODO
-      // store.createTag(name)
-    }
+  createTag() {
+    const name = window.prompt('请输入标签名');
+    if(!name){return window.alert('标签不能为空')}
+    this.$store.commit('createTag', name)
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.tags{
+.tags {
   background: white;
   font-size: 16px;
   padding-left: 16px;
-  > .tag{
+
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
     justify-content: space-between;
     border-bottom: 1px solid #e6e6e6;
-    svg{
+
+    svg {
       color: #666;
       margin-right: 16px;
       height: 18px;
