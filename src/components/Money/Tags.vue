d8498e1012c9a8bd0bc4bb2df82184ecbe329142
bens1320 <yu1piao09@163.com>
2020-10-07 14:10:47 +0800

    把所有 computed 改为 get

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -16,18 +16,16 @@ import {Component} from 'vue-property-decorator';
 import {mixins} from 'vue-class-component';
 import TagHelper from '@/mixins/TagHelper';
 
-@Component({
-  computed:{
-    tagList(){
-      return this.$store.state.tagList;
-    }
-  }
-})
+@Component
 export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
 
-  created(){
-    this.$store.commit('fetchTags')
+  get tagList() {
+    return this.$store.state.tagList;
+  }
+
+  created() {
+    this.$store.commit('fetchTags');
   }
 
   toggle(tag: string) {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -22,19 +22,18 @@ window.localStorage.setItem('version', '0.0.1');
 
 @Component({
   components: {Tags, FromItem, Types, NumberPad},
-  computed:{
-    recordList(){
-      return this.$store.state.recordList;
-    },
-  }
 })
 export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
   record: RecordItem = {
     tags: [], notes: '', type: '+', amount: 100
   };
 
-  created(){
-   this.$store.commit('fetchRecords')
+  created() {
+    this.$store.commit('fetchRecords');
   }
 
   onUpdateNotes(value: string) {
@@ -43,7 +42,7 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    this.$store.commit('createRecord', this.record)
+    this.$store.commit('createRecord', this.record);
   }
 
 }
