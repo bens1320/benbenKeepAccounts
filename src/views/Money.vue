f4b0596ab3e5e83fe2bb17097baf74e8fac78a0c
bens1320 <yu1piao09@163.com>
2020-10-10 11:11:19 +0800

    删除 Type 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,7 +14,6 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FromItem from '@/components/Money/FromItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
@@ -25,7 +24,7 @@ window.localStorage.setItem('version', '0.0.1');
 
 
 @Component({
-  components: {Tabs, Tags, FromItem, Types, NumberPad},
+  components: {Tabs, Tags, FromItem, NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {
