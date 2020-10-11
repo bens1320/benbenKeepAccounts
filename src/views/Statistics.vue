1d15b820d6235e895c5ec9fbcdaa736cee924b9a
bens1320 <yu1piao09@163.com>
2020-10-12 01:29:08 +0800

    Statistics 时间用day.js来处理

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,15 +3,15 @@
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
-      <li v-for="(group, index) in result" :key="index">
-        <h3 class="title">{{ group.title }}</h3>
+      <li v-for="(group) in result" :key="group.title">
+        <h3 class="title">{{ beautify(group.title) }}</h3>
         <ol>
-          <li v-for="(item, index) in group.items" :key="index"
+          <li v-for="item in group.items" :key="item.id"
               class="record"
           >
             <span>{{ tagString(item.tags) }}</span>
-            <span class="notes">{{item.notes}}</span>
-            <span>￥{{item.amount}}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
           </li>
         </ol>
       </li>
@@ -25,12 +25,30 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 import intervalList from '@/constants/intervalList';
+import dayjs from 'dayjs';
+
 
 @Component({
   components: {Tabs},
 
 })
 export default class Statistics extends Vue {
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
+
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',');
   }
