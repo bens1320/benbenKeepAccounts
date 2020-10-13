ac7ccc6e7879041d7589c4bb11f4fad080553195
bens1320 <yu1piao09@163.com>
2020-10-13 15:24:13 +0800

    完成 Statistics 页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,10 +1,9 @@
 <template>
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
       <li v-for="(group, index) in groupedList" :key="index">
-        <h3 class="title">{{ beautify(group.title) }}</h3>
+        <h3 class="title">{{ beautify(group.title) }} <span>￥{{group.total}}</span></h3>
         <ol>
           <li v-for="item in group.items" :key="item.id"
               class="record"
@@ -24,7 +23,6 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
-import intervalList from '@/constants/intervalList';
 import dayjs from 'dayjs';
 import clone from '@/lib/clone';
 
@@ -60,8 +58,12 @@ export default class Statistics extends Vue {
   get groupedList() {
     const {recordList} = this;
     if (recordList.length === 0) {return [];}
-    const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
-    const result = [{title: dayjs(newList[0].createdAt).format('YYYY-M-D'), items: [newList[0]]}];
+    const newList = clone(recordList)
+        .filter(r => r.type === this.type)
+        .sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+
+    type Result = {title: string; total?: number; items: RecordItem[]}[]
+    const result: Result = [{title: dayjs(newList[0].createdAt).format('YYYY-M-D'), items: [newList[0]]}];
     for (let i = 1; i < newList.length; i++) {
       const current = newList[i];
       const last = result[result.length - 1];
@@ -71,6 +73,10 @@ export default class Statistics extends Vue {
         result.push({title: dayjs(current.createdAt).format('YYYY-M-D'), items: [current]});
       }
     }
+    result.map(group => {
+      group.total = group.items.reduce((sum, item) => sum + item.amount, 0)
+    })
+    console.log(result);
     return result;
   }
 
@@ -80,10 +86,7 @@ export default class Statistics extends Vue {
 
 
   type = '-';
-  interval = 'day';
   recordTypeList = recordTypeList;
-  intervalList = intervalList;
-
 
 }
 </script>
@@ -111,10 +114,10 @@ export default class Statistics extends Vue {
 
 ::v-deep {
   .type-tabs-item {
-    background: white;
+    background: #c4c4c4;
 
     &.selected {
-      background: #c4c4c4;
+      background: white;
 
       &::after {
         display: none;
@@ -122,12 +125,5 @@ export default class Statistics extends Vue {
     }
   }
 
-  .interval-tabs-item {
-    height: 48px;
-  }
 }
-
-
-
-
 </style>
